5821d7ff45fdd5911a4afc1dcd9fe59a60a7a55f
RaphaelHardFork <raphael.pellet@protonmail.com>
2022-11-24 14:18:04 +0100

    init

---
+++ src/Metadata.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "openzeppelin-contracts/contracts/utils/Base64.sol";
+import "openzeppelin-contracts/contracts/utils/Strings.sol";
+
+contract Metadata {
+    function tokenURI(uint256 tokenId) external view returns (string memory) {
+        //
+    }
+}
